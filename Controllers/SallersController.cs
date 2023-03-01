3550ba1f0da3995da1f8dbf9d6eaeb80381e1ce7
Josué <gutillaj@gmail.com>
2023-03-01 00:30:28 -0400

    SallersController

---
+++ Controllers/SallersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VendasWebMvc.Controllers
+{
+    public class SallersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
